fe3802665dc540b6b27c211b36c2f8abc50af3a9
Olli Janatuinen <olli.janatuinen@gmail.com>
2019-01-06 21:40:09 +0200

    All build servers to download script

--- DownloadLogs.ps1
+++ DownloadLogs.ps1
@@ -1,18 +1,30 @@
-$rssAll = Invoke-WebRequest -UseBasicParsing -Uri https://jenkins.dockerproject.org/job/Docker-PRs/rssAll
-[xml]$xmlRssAll = $rssAll.Content
-$rssEntries = $xmlRssAll.feed.entry | Sort-Object published
+$rssURLs = @{
+	janky = "https://jenkins.dockerproject.org/job/Docker-PRs/rssAll";
+	experimental = "https://jenkins.dockerproject.org/job/Docker-PRs-experimental/rssAll";
+	powerpc = "https://jenkins.dockerproject.org/job/Docker-PRs-powerpc/rssAll";
+	z = "https://jenkins.dockerproject.org/job/Docker-PRs-s390x/rssAll";
+	rs1 = "https://jenkins.dockerproject.org/job/Docker-PRs-WoW-RS1/rssAll";
+	rs5 = "https://jenkins.dockerproject.org/job/Docker-PRs-WoW-RS5-Process/rssAll";
+}
 
-ForEach ($Row in $rssEntries) {
-	$Date = Get-Date -Date $row.published
-	$PrId = (($row.title -split "#")[1] -split " ")[0]
-	$BuildId = ($row.link.href -split "/")[5]
-	
-	If (!(Test-Path "$PSScriptRoot\$($Date.Year)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\$($Date.Year)" }
-	If (!(Test-Path "$PSScriptRoot\$($Date.Year)\$($Date.Month)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\$($Date.Year)\$($Date.Month)" }
-	If (!(Test-Path "$PSScriptRoot\$($Date.Year)\$($Date.Month)\$($Date.Day)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\$($Date.Year)\$($Date.Month)\$($Date.Day)" }
-	$FileName = "$PSScriptRoot\$($Date.Year)\$($Date.Month)\$($Date.Day)\PR" + $PrId + "_" + $BuildId + ".log"
+ForEach ($key in $rssURLs.Keys) {
+	$rssAll = Invoke-WebRequest -UseBasicParsing -Uri $rssURLs.Item($key)
 
-	If (!(Test-Path $FileName)) {
-		Invoke-WebRequest -UseBasicParsing -Uri $($row.link.href + "/consoleText") -OutFile $FileName
+	[xml]$xmlRssAll = $rssAll.Content
+	$rssEntries = $xmlRssAll.feed.entry | Sort-Object published
+
+	ForEach ($Row in $rssEntries) {
+		$Date = Get-Date -Date $row.published
+		$PrId = (($row.title -split "#")[1] -split " ")[0]
+		$BuildId = ($row.link.href -split "/")[5]
+
+		If (!(Test-Path "$PSScriptRoot\ci\$key\$($Date.Year)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\ci\$key\$($Date.Year)" }
+		If (!(Test-Path "$PSScriptRoot\ci\$key\$($Date.Year)\$($Date.Month)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\ci\$key\$($Date.Year)\$($Date.Month)" }
+		If (!(Test-Path "$PSScriptRoot\ci\$key\$($Date.Year)\$($Date.Month)\$($Date.Day)")) { New-Item -ItemType Directory -Path "$PSScriptRoot\ci\$key\$($Date.Year)\$($Date.Month)\$($Date.Day)" }
+		$FileName = "$PSScriptRoot\ci\$key\$($Date.Year)\$($Date.Month)\$($Date.Day)\PR" + $PrId + "_" + $BuildId + ".log"
+
+		If (!(Test-Path $FileName)) {
+			Invoke-WebRequest -UseBasicParsing -Uri $($row.link.href + "/consoleText") -OutFile $FileName
+		}
 	}
 }
